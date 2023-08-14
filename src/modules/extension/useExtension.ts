c036f6fda9c24c04627460b1106a66d7528c309d
Yegor Arndt <egor.kurochkin@int.alloysoftware.com>
2023-08-14 21:10:13 +0400

    Keep debugging for vercel

--- src/modules/extension/useExtension.ts
+++ src/modules/extension/useExtension.ts
@@ -1,26 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
-import { endpoint } from "./constants";
 import type { Vacancy } from "./types";
 
 export const useExtension = () => {
   const [data, setData] = useState<Vacancy | null>(null);
   const [isLoading, setLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await (await fetch(endpoint)).json();
-      } catch (err) {
-        console.error(err);
-        setError(err as Error);
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
 
   return { data, isLoading, error };
 };

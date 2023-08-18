487ae00ff95bf12a45f999393cae3ad736f07c6c
Yegor Arndt <egor.kurochkin@int.alloysoftware.com>
2023-08-18 15:27:50 +0400

    Make vercel happy

--- src/pages/api/extension/post.ts
+++ src/pages/api/extension/post.ts
@@ -1,27 +1,27 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import Cookies from "cookies";
 import jwt from "jsonwebtoken";
-//https://clerk.com/docs/request-authentication/validate-session-tokens?utm_source=www.google.com&utm_medium=referral&utm_campaign=none
-export default async function (req: NextApiRequest, res: NextApiResponse) {
-  const publicKey = process.env.CLERK_PEM_PUBLIC_KEY;
-  const cookies = new Cookies(req, res);
 
-  const sessToken = cookies.get("__session"); // the same origin
+//https://clerk.com/docs/request-authentication/validate-session-tokens?utm_source=www.google.com&utm_medium=referral&utm_campaign=none
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+  const publicKey = process.env.CLERK_PEM_PUBLIC_KEY as string;
+
+  /**
+   * You can extract the token from the request cookies or headers.
+   */
+  // const cookies = new Cookies(req, res);
+  // const sessToken = cookies.get("__session"); // the same origin
+
   const token = req.headers.authorization; // the extension origin
 
-  if (sessToken === undefined && token === undefined) {
+  if (!token) {
     res.status(401).json({ error: "not signed in" });
     return;
   }
 
   try {
-    let decoded = "";
     if (token) {
-      decoded = jwt.verify(token, publicKey);
-      res.status(200).json({ sessToken: decoded });
-      return;
-    } else {
-      decoded = jwt.verify(sessToken, publicKey);
+      const decoded = jwt.verify(token, publicKey);
       res.status(200).json({ sessToken: decoded });
       return;
     }

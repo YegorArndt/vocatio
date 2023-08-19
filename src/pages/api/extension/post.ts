891f394bdaf0c0732e3705d402eeded58313bb2d
Yegor Arndt <egor.kurochkin@int.alloysoftware.com>
2023-08-19 17:46:36 +0400

    Crossdomain auth

--- src/pages/api/extension/post.ts
+++ src/pages/api/extension/post.ts
@@ -1,7 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import jwt from "jsonwebtoken";
 import fs from "fs";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 const publicKey = fs.readFileSync("secret/public.pem", "utf8");
 
 type Err = {
@@ -9,19 +11,29 @@ type Err = {
   message: string;
 };
 
-export default function (req: NextApiRequest, res: NextApiResponse) {
+export default async function (req: NextApiRequest, res: NextApiResponse) {
   const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
     return res.status(401).json({ message: "Unauthorized" });
   }
 
-  /**
-   * Verify token
-   */
   try {
     if (!publicKey) throw new Error("Public key not found");
+
+    /**
+     * Verify token
+     */
     jwt.verify(token, publicKey);
+
+    /**
+     * Push to database
+     */
+    const { data } = req.body;
+    //log data type
+    console.log(data);
+    // await prisma.vacancy.create({ data });
+
     res.status(200).json({ message: "Success" });
   } catch (error) {
     res.status(401).json({ message: (error as Err).message });

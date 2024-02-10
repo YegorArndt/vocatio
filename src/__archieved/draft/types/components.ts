a7a87eb9835a7d23861903a67c6c03420757bb81
Yegor Arndt <egor.kurochkin@int.alloysoftware.com>
2024-02-10 18:28:24 +0400

    Temp commit, work in progress

--- src/__archieved/draft/types/components.ts
+++ src/__archieved/draft/types/components.ts
@@ -1,42 +1,42 @@
-import { type CSSProperties } from "react";
+// import { type CSSProperties } from "react";
 
-import type { SectionId } from "./sections";
-import type { DndProviderProps } from "~/modules/create/design/baseComponents/DndProvider";
-import { GroupProps } from "~/modules/create/design/baseComponents/Group";
-import { GeneratedDraft } from ".";
+// import type { SectionId } from "./sections";
+// import type { DndProviderProps } from "~/modules/create/design/baseComponents/DndProvider";
+// import { GroupProps } from "~/modules/create/design/baseComponents/Group";
+// import { GeneratedDraft } from ".";
 
-/**
- * Each component must have: value, label, icon, type, id.
- */
+// /**
+//  * Each component must have: value, label, icon, type, id.
+//  */
 
-export type PropsInitializer = (
-  data: GeneratedDraft
-) => Partial<NormalizedProps>;
+// export type PropsInitializer = (
+//   data: GeneratedDraft
+// ) => Partial<NormalizedProps>;
 
-export type NormalizedProps = Partial<GroupProps & DndProviderProps> & {
-  className: string;
-  style: CSSProperties;
-  tooltip?: string;
-};
+// export type NormalizedProps = Partial<GroupProps & DndProviderProps> & {
+//   className: string;
+//   style: CSSProperties;
+//   tooltip?: string;
+// };
 
-export type NormalizedComponent = {
-  id: string;
-  type: NormalizedType;
-  sectionId: SectionId;
-  props: NormalizedProps;
-};
+// export type NormalizedComponent = {
+//   id: string;
+//   type: NormalizedType;
+//   sectionId: SectionId;
+//   props: NormalizedProps;
+// };
 
-export type NormalizedType =
-  | "text"
-  | `heading-${number}`
-  | "group"
-  | "divider"
-  | "image"
-  | "icon-group"
-  | "entries"
-  | "context"
-  | "contact"
-  | "experience"
-  | "education"
-  | "skills"
-  | "languages";
+// export type NormalizedType =
+//   | "text"
+//   | `heading-${number}`
+//   | "group"
+//   | "divider"
+//   | "image"
+//   | "icon-group"
+//   | "entries"
+//   | "context"
+//   | "contact"
+//   | "experience"
+//   | "education"
+//   | "skills"
+//   | "languages";
